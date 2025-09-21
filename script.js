38ea3f6bb03846b8699b5c63ee023ef261859ef6
ignacioFiori <nacho.fiori@hotmail.com.ar>
2025-09-21 17:45:39 -0300

    Se desarrolla scripts para que el formulario envie email con emailjs, se agrega link de whatsapp, cambio de font entre otras cosas

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function enviar() {
-  // Tomar valores
+function enviar(e) {
+  e.preventDefault(); 
+
   const nombre  = document.getElementById("nombre").value.trim();
   const email   = document.getElementById("email").value.trim();
   const mensaje = document.getElementById("mensaje").value.trim();
 
-  // Validaciones básicas
   if (!nombre) {
     alert("Por favor, ingresá tu nombre.");
     return;
@@ -13,20 +13,35 @@ function enviar() {
     alert("Por favor, ingresá tu email.");
     return;
   }
-
+    //validación de formato de email con regex
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert("Por favor, ingresá un email válido (ej: nombre@dominio.com).");
+    return;
+  }
   if (!mensaje) {
     alert("Por favor, escribí un mensaje.");
     return;
   }
 
-  // Si todo está bien, mostrar contenido
-  alert(
-    `Nombre: ${nombre}\nEmail: ${email}\nMensaje: ${mensaje}`
-  );
 
-  // limpiar los campos
-  document.getElementById("form-contacto").reset();
+  emailjs.send("service_yj6yfl1", "template_uatoise", {
+    nombre: nombre,
+    email: email,
+    mensaje: mensaje
+  })
+  .then(function(response) {
+      console.log("SUCCESS!", response.status, response.text);
+      alert("Mensaje enviado correctamente.Me estaré comunicando con usted en breve ¡Gracias por contactarme!");
+      
+  
+      document.getElementById("form-contacto").reset();
+      
 
-  //scroll a inicio
-  document.getElementById("inicio").scrollIntoView({ behavior: "smooth" });
+      document.getElementById("inicio").scrollIntoView({ behavior: "smooth" });
+  })
+  .catch(function(error) {
+      console.error("FAILED...", error);
+      alert(" Hubo un problema al enviar el mensaje. Intentalo de nuevo más tarde.");
+  });
 }
